bf6725acc102d101afe96586e83619018d93896c
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-06-06 01:01:55 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,5 @@
-// console.log("EXECUTED");
+import dotenv from 'dotenv';
+dotenv.config();
 
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
-
-// const person: string = 'Martin';
-// const count: number = 100;
-
-
-
-
-
-
-// Architectural pattern: MVC, DI, MVP
-
-// MVC => MODEL VIEW CONTROLLER
-
-// Design pattern: Middleware, Decotar
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
