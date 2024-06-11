e31f3e09f343378248a0bc19057815886e14d35e
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-06-11 13:37:45 +0900

    feat: create project standarts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./routerAdmin"
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -9,6 +11,7 @@ console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSION **/
 /** 3-VIEWS **/
@@ -20,6 +23,6 @@ app.set("view engine", "ejs");
 // 1 SPA: REACT
 // 2 BSSR: EJS backendda froneendni togridan togri qurib olishda yordam bermoqda
 app.use('/admin', routerAdmin);  //EJS
-app.use('/', router);   // REACT Middleware Design Pattern
+app.use('/', router);   // REACT (Middleware Design Pattern)  (kelayotgan requestlarni ruterga yuborishini belgilayabmiz)
 
 export default app;
