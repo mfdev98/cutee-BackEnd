c3065059268c3a5501b193a2a1f60f9a6e5fbd4e
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-06-19 06:48:19 +0900

    feat: frontend related css, js and image creation

--- src/controllers/restaurant.controller .ts
+++ src/controllers/restaurant.controller .ts
@@ -9,7 +9,7 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
    try {
       console.log("goHome");
-      res.send("Home Page");
+      res.render("home");
    } catch (err) {
       console.log("Error, goHome", err);
    }
@@ -18,7 +18,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
    try {
       console.log("getSignup");
-      res.send("SignUp Page");
+      res.render("signup");
    } catch (err) {
       console.log("Error, SignUp", err);
 
@@ -28,7 +28,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
    try {
       console.log("getLogin");
-      res.send("Login Page");
+      res.render("login");
    } catch (err) {
       console.log("Error, getLogin", err);
    }
