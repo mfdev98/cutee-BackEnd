f709505e0db0cda6ebbc3e6318c10211eed9f7e0
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-06-29 20:51:18 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,23 @@
 console.log("Signup frontend javascript file");
+
+function validateSignupForm() {
+   const memberNick = $(".member-nick").val();
+   const memberPhone = $(".member-phone").val();
+   const memberPaswword = $(".member-password").val();
+   const confirmPassword = $(".confirm-password").val();
+
+   if (
+      memberNick === "" ||
+      memberPhone === "" ||
+      memberPaswword === "" ||
+      confirmPassword === ""
+   ) {
+      alert("Please insert all required inputs");
+      return false;
+   }
+
+   if (memberPaswword !== confirmPassword){
+      alert("Password differs, Please check!");
+      return false;
+   }
+}
